99f3a89a541d2a4de40d6fd4461998c5340c39e8
Jarek K <jarokaz@hotmail.com>
2018-03-09 17:01:25 -0800

    updates

--- scripts/preprocess_sound.py
+++ scripts/preprocess_sound.py
@@ -8,59 +8,34 @@ import numpy as np
 import pandas as pd 
 
 
-def is_overlap(rec1, rec2, margin):
-    """
-    Returns true if rectangles overlap; false otherwise
-    
-    Parameters
-    ----------
-    rec1: [min_x, min_y, max_x, max_y]
-    rec2: [min_x, min_y, max_x, max_y]
-    margin: overlap margin
-    """
-
-    overlap = False
-
-    w = rec1[2] - rec1[0]
-    h = rec1[3] - rec1[1]
-
-    x1 = rec2[0] - rec1[0]
-    y1 = rec2[1] - rec1[1]
-    x2 = rec2[2] - rec1[0]
-    y2 = rec2[3] - rec1[1]
-
-    if (x1 > -margin) and (x1 < w) and (y1 > -margin) and (y1 < h) and (x2 < margin + w) and (y2 < margin + h):
-       overlap = True
-
-    return overlap
-        
-
-def clone_blemishes(image, group, size, stride, outputpath, suffix, file_number, file_type):
-    min_x = min(group['min_x'])
-    min_y = min(group['min_y'])
-    max_x = max(group['max_x'])
-    max_y = max(group['max_y'])
-    
-    group = group[group['label'] != 'sound']
-    
+def overlay_grid(image, group):
+    draw = ImageDraw.Draw(image)
     for row in group.iterrows():
-        x1, y1, x2, y2 = row[1][2], row[1][1], row[1][4], row[1][3]
-        x = x_start = max(min_x, x2 - size)
-        y = y_start = max(min_y, y2 - size)
-        x_end = min(x1, max_x - size)
-        y_end = min(y1, max_y - size)
-        folder = row[1][5]
+        cor = [row[1][2], row[1][1], row[1][4], row[1][3]]
+        draw.rectangle(cor)
+    return image
 
-        while x <= x_end:
-            while y <= y_end:
+def clone_sound_samples(image, group, size, prefix, outputpath, file_type):
+        
+    group = group.sort_values(['min_x', 'min_y'])
+    group = group.reset_index(drop=True)
+
+    cols = group['min_x'].nunique()
+    rows = group['min_y'].nunique()
+    prefix = prefix + '_' + group.iloc[0]['image'] + '_sound_'
+    outputpath = join(outputpath, 'sound')
+    filenumber = 1
+    for col in range(cols-1):
+        for row in range(rows-1):
+            segments = [group.iloc[i]['label'] for i in [col*rows + row, (col+1)*rows + row, col*rows + row + 1 , (col+1)*rows + row + 1]]
+            if segments.count('sound') == 4:
+                x = group.iloc[col*rows + row]['min_x'] + 10
+                y = group.iloc[col*rows + row]['min_y'] + 5
                 im = image.crop((x, y, x + size, y + size))
-                filename = join(outputpath, folder, suffix + '_' + str(file_number) + file_type)
+                filename = join(outputpath, prefix + str(filenumber) + file_type)
                 im.save(filename)
-                file_number += 1
-                y = y + stride
-            y = y_start
-            x = x + stride        
-    return file_number
+                filenumber += 1
+        
 
 
 index = "c:/repos/lumber/data/unprocessed/manlabel.txt"
@@ -68,18 +43,19 @@ df = pd.read_csv(index, delim_whitespace=True, header=None, names=['image','min_
 gb = df.groupby(df['image']) 
 
 file_type = ".PNG"
+prefix = 'train'
 inputpath="c:/repos/lumber/data/unprocessed"
 outputpath="c:/repos/lumber/data/processed"
 
 clone_size = 100
-stride = 10
-file_number = 1
 
 for name, group in gb:
     filepath = join(inputpath, name)
     image = Image.open(filepath)
-    file_number = clone_blemishes(image, group, clone_size, stride, outputpath, name, file_number, file_type)
-    print(name)
+    clone_sound_samples(image, group, clone_size, prefix, outputpath, file_type)
+    
+
+
                
 
         
