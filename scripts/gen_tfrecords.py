91a2e5b0adf9fc5a261ecbb4fc62686f3743c678
Ubuntu <demouser@jkp40.jf1s0eh221yezisemv3ur43ccd.xx.internal.cloudapp.net>
2018-03-14 01:01:05 +0000

    update

--- scripts/gen_tfrecords.py
+++ scripts/gen_tfrecords.py
@@ -9,37 +9,6 @@ from sklearn.model_selection import train_test_split
 
 
 
-def load_image(path, name):
-    im = Image.open(join(path, name))
-    return np.array(im)
-
-
-def _int64_feature(value):
-  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-
-def _bytes_feature(value):
-  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-
-
-    
-def convert_to_tfrecord(labels, inputpath, outputfile):    
-    with tf.python_io.TFRecordWriter(outputfile) as writer:
-        counter = 0
-        for index, row in labels.iterrows():
-            im = load_image(inputpath, row['image'])
-            example= tf.train.Example(
-                features = tf.train.Features(
-                    feature = {
-                        'image': _bytes_feature(im.tostring()),
-                        'label': _int64_feature(row['label'])
-                    }))
-            writer.write(example.SerializeToString())
-            counter += 1
-            if counter%500 == 0:
-                print("Processed {0} images.".format(counter))
-                      
- 
 
 
 def clone_sound_samples(image, group, size, outputpath, file_type):        
@@ -47,7 +16,7 @@ def clone_sound_samples(image, group, size, outputpath, file_type):
     group = group.reset_index(drop=True)
     cols = group['min_x'].nunique()
     rows = group['min_y'].nunique()
-    label = 'sound'
+    label = ALLOWED_LABELS[0]
     prefix = group.iloc[0]['image'] + '_' + label + '_'
     filenumber = 1
     for col in range(cols-1):
@@ -65,8 +34,8 @@ def clone_sound_samples(image, group, size, outputpath, file_type):
         
 
 def generate_sound_samples(df, inputpath, outputpath):
-    clone_size = 112
-    file_type = ".PNG"
+    clone_size = SNAPSHOT_SIZE
+    file_type = SNAPSHOT_FILETYPE
     gb = df.groupby(df['image'])
     for name, group in gb:
         filepath = join(inputpath, name)
@@ -111,9 +80,9 @@ def clone_blemishes(image, group, size, stride,outputpath, file_type):
 
 
 def generate_blemished_samples(df, inputpath, outputpath):
-    clone_size = 112
-    stride = 10
-    file_type = ".PNG"
+    clone_size = SNAPSHOT_SIZE
+    stride = SNAPSHOT_STRIDE
+    file_type = SNAPSHOT_FILETYPE
     gb = df.groupby(df['image'])
     for name, group in gb:
         filepath = join(inputpath, name)
@@ -163,10 +132,69 @@ def generate_snapshots(index_path, image_dir, snapshots_dir, testing_percentage)
     generate_blemished_samples(testing, image_dir, join(snapshots_dir, TESTING_SUBFOLDER_NAME))
     print("Generated {0} testing snapshots".format(len(os.listdir(join(snapshots_dir, TESTING_SUBFOLDER_NAME)))))
     
+
+    
+def load_image(path, name):
+    im = Image.open(join(path, name))
+    return np.array(im)
+
+
+def _int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+    
+def convert_to_tfrecord(images, inputpath, outputfile):    
+    
+    with tf.python_io.TFRecordWriter(outputfile) as writer:
+        for name, label in images:
+            im = load_image(inputpath, name)
+            example= tf.train.Example(
+                features = tf.train.Features(
+                    feature = {
+                        'image': _bytes_feature(im.tostring()),
+                        'label': _int64_feature(label)
+                    }))
+            writer.write(example.SerializeToString())
+           
+def extract_label(filename):
+    label = filename[filename.find('_')+1 : filename.rfind('_')]
+    return label
+    
+def prepare_image_label_list(image_folder):
+    images = os.listdir(image_folder)
+    images =[(image, ALLOWED_LABELS.index(extract_label(image))) for image in images if extract_label(image) in ALLOWED_LABELS]
+    return images
+    
+def generate_tfrecords(snaphots_dir, output_dir, validation_percentage):
+    
+    development_folder = join(snaphots_dir, DEVELOPMENT_SUBFOLDER_NAME)
+    development_images = prepare_image_label_list(development_folder)
+    testing_folder = join(snaphots_dir, TESTING_SUBFOLDER_NAME)
+    testing_images = prepare_image_label_list(testing_folder)
+    
+    print("Total number of development images is {0}".format(len(development_images)))
+    print("Total number of testing images is {0}".format(len(testing_images)))
+        
+    # Split development set into training and validattion
+    last_training_index = len(development_images)
+    last_validation_index = round(last_training_index * validation_percentage/100)
+    
+    validation_images = development_images[0: last_validation_index]
+    training_images = development_images[last_validation_index: last_training_index]
+    
+    print("Generating training TFRecords file using {0} images".format(len(training_images)))
+    convert_to_tfrecord(training_images, development_folder, join(output_dir, TRAINING_FILE_NAME))
+    
+    print("Generating validation TFRecords file using {0} images".format(len(validation_images)))
+    convert_to_tfrecord(validation_images, development_folder, join(output_dir, VALIDATION_FILE_NAME))
+     
+    print("Generating testing TFRecords file using {0} images".format(len(testing_images)))
+    convert_to_tfrecord(testing_images, testing_folder, join(output_dir, TESTING_FILE_NAME))
    
-    
-    
-    
 
     
     
@@ -177,9 +205,14 @@ DEVELOPMENT_SUBFOLDER_NAME = 'development'
 SNAPSHOT_SIZE = 112
 SNAPSHOT_FILETYPE = ".PNG"
 SNAPSHOT_STRIDE = 10
+ALLOWED_LABELS = ['sound', 'split', 'wane', 'sound_knot', 'dry_knot', 'core_stripe', 'small_knot']
+TRAINING_FILE_NAME = 'training.tfrecords'
+VALIDATION_FILE_NAME = 'validation.tfrecords'
+TESTING_FILE_NAME = 'testing.tfrecords'
+
 
     
-def main(image_dir, output_dir, snapshots_dir, validation_percentage, testing_percentage):
+def main(image_dir, output_dir, snapshots_dir, validation_percentage, testing_percentage, action):
     if not os.path.isdir(image_dir):
         print("Image directory does not exist !!!")
         return
@@ -204,6 +237,10 @@ def main(image_dir, output_dir, snapshots_dir, validation_percentage, testing_pe
         print("Snapshot folders not empty")
         return
     
+    if len(os.listdir(output_dir)) != 0:
+        print("TFRecords folders not empty")
+        return
+    
     if validation_percentage + testing_percentage > 50:
         print("Wrong split proportions. Think again...")
         return
@@ -212,13 +249,33 @@ def main(image_dir, output_dir, snapshots_dir, validation_percentage, testing_pe
         print("Missing manlabel.txt file from image directory")
         return
     
+          
     print("Image directory: {0}".format(image_dir))
-    print("TFRecords directory: {0}".format(output_dir))      
-    print("Percentage of images used for the validation set: {0}".format(validation_percentage))
-    print("Percentage of images used for the testing set: {0}".format(testing_percentage))
     
-    generate_snapshots(join(image_dir, ORIGINAL_LABELS_FILENAME), image_dir, snapshots_dir, testing_percentage)
-       
+    if action == 'tfrecords':
+      print("Only TFRecords will be generated")
+      print("TFRecords directory: {0}".format(output_dir))
+      print("Percentage of images used for the training set: {0}".format(100 - validation_percentage))
+      print("Percentage of images used for the validation set: {0}".format(validation_percentage))
+      generate_tfrecords(snapshots_dir, output_dir, validation_percentage)
+    elif action == 'snapshots':
+      print("Only snapshots will be generated")
+      print("Percentage of unprocessd images used for the development set: {0}".format(100 - testing_percentage))
+      print("Percentage of unprocessed images used for the testing set: {0}".format(testing_percentage))
+      generate_snapshots(join(image_dir, ORIGINAL_LABELS_FILENAME), image_dir, snapshots_dir, testing_percentage)
+    elif action == 'both':   
+      print("Both snapshots and tfrecords will be generated")
+      print("TFRecords directory: {0}".format(output_dir))
+      print("Snapshots directory: {0}".format(snapshots_dir))
+      print("Percentage of unprocessed images used for the development set: {0}".format(100 - testing_percentage))
+      print("Percentage of unprocessed images used for the testing set: {0}".format(testing_percentage))
+      print("Percentage of images used for the training set: {0}".format(100 - validation_percentage))
+      print("Percentage of images used for the validation set: {0}".format(validation_percentage))
+      generate_snapshots(join(image_dir, ORIGINAL_LABELS_FILENAME), image_dir, snapshots_dir, testing_percentage)
+      generate_tfrecords(snapshots_dir, output_dir, validation_percentage)
+    else:
+      print("Wrong action specified")
+
     
     
     
@@ -255,8 +312,13 @@ if __name__ == '__main__':
       type=int,
       default=10,
       help='Percentage of images to use as a test set')
+  parser.add_argument(
+      '--action',
+      type=str,
+      default='tfrecords',
+      help='Generate tf files: tfrecords; generate snapshots: snapshots; generate both: both')
   
   
 
   args = parser.parse_args()
-  main(args.image_dir, args.output_dir, args.snapshots_dir, args.validation_percentage, args.testing_percentage)
+  main(args.image_dir, args.output_dir, args.snapshots_dir, args.validation_percentage, args.testing_percentage, args.action)
